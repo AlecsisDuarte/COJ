8919ce550289efe744b28fa3b638a405e2fc7f5a
Alexis Duarte <alexis.duarte@hotmail.com>
2019-07-31 17:03:40 -0700

    fixed

--- java/_1197_MegaSearcher.java
+++ java/_1197_MegaSearcher.java
@@ -3,27 +3,27 @@
  *
  * Description:
  *  The Intranet, the vast network that feeds us, contains much information. So,
- *  it is not possible to find a particular file quickly. To resolve this problem
- *  arose network scanners, those wonderful tools that let you painlessly find any
- *  series, movie, game or photo you want "as we work, of course:)". Then, in view
- *  of this problem, Enrique decided to make a scanner, to demonstrate that it is
- *  not so difficult. The first version was a success, but soon received
- *  complaints from users who wanted to always know how long it takes to finish
- *  the search. In trying to resolve this problem, however, found a complication
- *  in the application module in charge of finding the computer on, because it was
- *  necessary to know the number of available IP addresses in the range to search.
- *  Enrique can not resolve this difficult problem, but is too proud to admit it.
- *  Can you help ?
+ *  it is not possible to find a particular file quickly. To resolve this
+ *  problem arose network scanners, those wonderful tools that let you painlessly
+ *  find any series, movie, game or photo you want "as we work, of course:)".
+ *  Then, in view of this problem, Enrique decided to make a scanner, to
+ *  demonstrate that it is not so difficult. The first version was a success, but
+ *  soon received complaints from users who wanted to always know how long it
+ *  takes to finish the search. In trying to resolve this problem, however, found
+ *  a complication in the application module in charge of finding the computer
+ *  on, because it was necessary to know the number of available IP addresses in
+ *  the range to search. Enrique can not resolve this difficult problem, but is
+ *  too proud to admit it. Can you help ?
  *
  * Input specification:
  *  An IPv4 address as used in Cuba is in the form X.X.X.X, where X is a number
  *  between 0 and 255. Some of these are used for routing IP network to various
  *  methods of communication between servers and active elements and change
  *  depending on the configuration of the network. Some are reserved and have
- *  special meanings depending on the class to which they belong. For purposes of
- *  this problem, we assume that all IPs are valid and the Mega Searcher can be
- *  used with any kind of IP address. The entry consists of a range of IPs, that
- *  is, two valid IP addresses A and B (A > B), one in each input line.
+ *  special meanings depending on the class to which they belong. For purposes
+ *  of this problem, we assume that all IPs are valid and the Mega Searcher can
+ *  be used with any kind of IP address. The entry consists of a range of IPs,
+ *  that is, two valid IP addresses A and B (A > B), one in each input line.
  *
  * Output specification:
  *  The output is a single number, the number of IPs theoretically valid in the
@@ -57,9 +57,9 @@ public class _1197_MegaSearcher {
   static long getTotalIPs(String[] ipsA, String[] ipsB) {
     long a = Integer.parseInt(ipsA[0]), b = Integer.parseInt(ipsB[0]);
 
-    for (int i = 0; i < 4; i++) {
-      a = (a * 256) + Integer.parseInt(ipsA[i]);
-      b = (b * 256) + Integer.parseInt(ipsB[i]);
+    for (int i = 1; i < 4; i++) {
+      a = (a * 256) + Long.parseLong(ipsA[i]);
+      b = (b * 256) + Long.parseLong(ipsB[i]);
     }
 
     return a - b + 1;
