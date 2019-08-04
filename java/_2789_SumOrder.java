e8e9d3f3933b4c6fb0d888a2fdb1acb61090b828
Alexis Duarte <alexis.duarte@hotmail.com>
2019-08-03 19:28:47 -0700

    added documentation

--- java/_2789_SumOrder.java
+++ java/_2789_SumOrder.java
@@ -1,3 +1,32 @@
+/**
+ * Problem:
+ *
+ * Description:
+ *  Paul is learning how to add numbers. To make the calculation easier, the sum
+ *   only contains numbers 1, 2 and 3. He is only beginning to count, so he can
+ *   calculate a sum only if the summands follow in non-decreasing order. For
+ *   example, he can't add the sum 1+3+1 but he can calculate sums 1+1+2 and 3+3.
+ *   Given the sum, your task is to rearrange the summands in such way that Paul can
+ *   make the sum calculation.
+ *  
+ *
+ * Input specification:
+ *  The first line contains a non-empty string s, the sum Paul needs to count.
+ *  String s contains no spaces. It only contains digits and characters "+".
+ *  Besides, string s is a correct sum of numbers 1, 2 and 3. The length of the
+ *  string s is less than or equal than 1000 characters.
+ *
+ * Output specification:
+ *  Print the sum after rearranging the summands belonging to the input.
+ *
+ * Sample input:
+ *  3+2+1
+ *
+ * Sample output:
+ *  1+2+3
+ *
+ * URL: http://coj.uci.cu/24h/problem.xhtml?pid=2789
+ */
 import java.util.Arrays;
 import java.util.Scanner;
 
